ef25b7b8d8374ffb842b967ddac1877a7ac102e8
Leslie Passante <leslie.passante@gmail.com>
2019-09-01 17:57:52 +0900

    generate json config in webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+if (!process.env.MAPBOX_ACCESS_TOKEN) {
+  console.error('Missing required environment variable MAPBOX_ACCESS_TOKEN.');
+  process.exit(1);
+}
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
